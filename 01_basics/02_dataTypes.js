053243cd781dbe8a399349d3770cfd92e11d969a
Advayaverma <advaya2208@gmail.com>
2025-07-01 01:47:43 +0530

    first concept complete

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Advaya")
 
 
-let name = "hitesh"
+let name = "Advaya"
 let age = 18
 let isLoggedIn = false
 let state;

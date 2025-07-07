b7126fb2fb5806569479830bee93fb63d2d5d353
Advayaverma <advaya2208@gmail.com>
2025-07-07 17:17:57 +0530

    dates and time

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -7,14 +7,15 @@ let myDate = new Date()
 // console.log(myDate.toISOString());
 // console.log(myDate.toJSON());
 // console.log(myDate.toLocaleTimeString());
-console.log(myDate.toTimeString());
+// console.log(myDate.toTimeString());
+
 
 console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date(2025, 0, 20)   /* the last number declares the no of days in year */
+// let myCreatedDate = new Date(2025, 6, 7, 4, 13) /* this declares time as well */
+// let myCreatedDate = new Date("2025-06-24")
+// let myCreatedDate = new Date("07-15-2025")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
@@ -24,14 +25,14 @@ let myTimeStamp = Date.now()
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
+console.log(newDate.toLocaleString('default', {
+    weekday: "short"
     
-})
+}))
 

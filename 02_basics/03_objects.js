cb941d2429e4af82a877279552d245e683f72dbf
Advayaverma <advaya2208@gmail.com>
2025-07-09 23:04:58 +0530

    objects part 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,38 +1,41 @@
-// singleton
-// Object.create
+// singleton  /**only one object from constructor */
+// Object.create  /**singleton method */
+
+
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySymbol = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    name: "Advaya",  /**name is stored in the form of string i.e. "name" */
+    "full name": "Advaya Verma",  /**this cannot be accessed by using `.`because it is defined as string */
+    [mySymbol]: "mykey1",
+    age: 19,
+    location: "Faridabad",
+    email: "abcd@gmail.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 
 // console.log(JsUser.email)
+// console.log(JsUser[email]);  /**this will not work because email should be in string format */
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser[mySymbol])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "abcd@hotmail.com"
+// Object.freeze(JsUser)  /**to freeze the changes to the object */
+JsUser.email = "abcd@yahoo.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+JsUser.greeting2 = function(){
+    console.log(`Hello JS user, ${this.name}`);  /**this is used to refer to the same object */
 }
 
 console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting2());
